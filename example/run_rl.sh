7194ea56474b71915bd87ec109618dcc1abf5b23
buchio <k_techevo@yahoo.co.jp>
2020-01-30 13:57:57 +0900

    add some utils for work on jupyter

--- example/run_rl.sh
+++ example/run_rl.sh
@@ -1,8 +1,9 @@
 python run_behavior_clone.py \
---env_name 'HopperPyBulletEnv-v0' \
+--env_name 'gym_flatworld:Flatworld-v0' \
 --num_parallel 4 \
 --cuda 0 \
 --record \
---expert_fname "HopperPyBulletEnv-v0_max.pkl" \
+--expert_dir "/home/hirobuchi.ryota/rl_lab/GAILs/data" \
+--expert_fname "expert_gym_flatworld:Flatworld-v0_10_s42.pkl" \
 
 # --max_epis 100 \
